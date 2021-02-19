db66bf2d870700291d871d62633dacfb873a8127
arun436 <arunkumarchintha136@gmail.com>
2021-02-19 12:43:30 +0530

    Added Error and removed resetvalues()

--- src/components/App.js
+++ src/components/App.js
@@ -32,19 +32,22 @@ const App = () => {
     setValues({...values,password: event.target.value});
   };
 
-  const resetValues = () =>{
-    values.name = "";
-    values.email = "";
-    values.phonenumber = "";
-    values.gender = "male";
-    values.password = "";
-  }
+  // const resetValues = () =>{
+  //   let thisInterval = setInterval(()=>
+  //   {
+  //     values.name = "";
+  //     values.email = "";
+  //     values.phonenumber = "";
+  //     values.gender = "male";
+  //     values.password = "";
+  //   },200);
+  //   clearInterval(thisInterval);
+  // }
   const handleSubmit = (event) => {
     event.preventDefault();
     if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
     {
       setValid(true);
-      resetValues();
     }
     setSubmitted(true);
   }
@@ -52,6 +55,7 @@ const App = () => {
     <div id="main">
       <form id="register-form" onSubmit={handleSubmit}>
         {submitted && valid ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
+        {/* {submitted && valid ? resetValues() : null} */}
         <input data-testid='name' className="form-field" onChange={handleNameInputChange} 
         value={values.name}
         type="text"
@@ -59,7 +63,7 @@ const App = () => {
         name="Name"
         required>
         </input>
-        {/* {submitted && !values.name ? <span>Name Error</span> : null} */}
+        {submitted && !values.name ? <span>Name Error</span> : null}
         {submitted && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
         <input data-testid='email' className="form-field" value={values.email} onChange={handleEmailInputChange}
         type="email"
@@ -67,7 +71,7 @@ const App = () => {
         name="email"
         required>
         </input>
-        {/* {submitted && !values.email ? <span>Email Error</span> : null} */}
+        {submitted && !values.email ? <span>Email Error</span> : null}
         {submitted && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
         <select data-testid='gender' className="form-field" value={values.gender} onChange={handleGenderInputChange}>
             <option defaultValue="male">male</option>
@@ -75,15 +79,14 @@ const App = () => {
             <option value="others">others</option>
         </select>
         <input data-testid='phoneNumber' required className="form-field" value={values.phonenumber} onChange={handlePhoneNumberInputChange} type="number" placeholder="phone"></input>
-        {/* {submitted && !values.phonenumber ? <span>Phone Number Error</span> : null} */}
+        {submitted && !values.phonenumber ? <span>Phone Number Error</span> : null}
         <input data-testid='password' required id="password" type='password' className="form-field" value={values.password} onChange={handlePasswordInputChange} placeholder="password"></input>
-        {/* {submitted && !values.password ? <span>Password Error</span> : null} */}
+        {submitted && !values.password ? <span>Password Error</span> : null}
         {submitted && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
         <button data-testid='submit' className="form-field" id="submit" placeholder="Submit">Submit</button>
       </form>
     </div>
   )
 }
-
 
 export default App;
